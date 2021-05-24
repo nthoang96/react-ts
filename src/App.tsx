dc3ad8533e3ade66ad86d8712929ed4369bd9738
nthoang96 <hoanglion.it@gmail.com>
2021-05-24 22:31:53 +0700

    appbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
 
-function App() {
+//Material UI
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+
+const customTheme = createMuiTheme({
+  palette: {
+    background: {
+      paper: "#ffdbdc",
+    },
+    primary: {
+      main: "#3a4664",
+    },
+    secondary: {
+      main: "#eaa0a2",
+    },
+  },
+});
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={customTheme}>
+      <Header />
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { makeStyles, Theme, withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+}));
+
+const StyledTab = withStyles({
+  root: {
+    fontWeight: "bold",
+  },
+})(Tab);
+
+export default function SimpleTabs() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="inherit">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="secondary"
+          centered
+        >
+          <StyledTab label="I'm a wibu" />
+          <StyledTab label="Functional Component Excercise" />
+        </Tabs>
+      </AppBar>
+    </div>
+  );
+}
